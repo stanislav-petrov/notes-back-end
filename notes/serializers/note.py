38bb3434947c85ec2b70db329cf0207f27b3e69e
stanislav-petrov <stanislavpetrov025@gmail.com>
2022-03-23 20:22:08 +0200

    Add creator to the note fields

--- notes/serializers/note.py
+++ notes/serializers/note.py
@@ -2,10 +2,13 @@
 from notes.models import Note
 from rest_framework import serializers
 
+from notes.serializers.user import UserSerializer
+
 
 class NoteSerializer(serializers.ModelSerializer):
     content = serializers.CharField()
+    creator = UserSerializer()
 
     class Meta:
         model = Note
-        fields = ('content', )
+        fields = ('content', 'creator')
