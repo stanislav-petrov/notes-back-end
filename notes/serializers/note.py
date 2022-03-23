727364c0ec8dc77fb3888ed896e1c73dfd2540fb
stanislav-petrov <stanislavpetrov025@gmail.com>
2022-03-23 20:53:10 +0200

    Include content validation

--- notes/serializers/note.py
+++ notes/serializers/note.py
@@ -1,14 +1,27 @@
+import re
 
 from notes.models import Note
 from rest_framework import serializers
 
-from notes.serializers.user import UserSerializer
-
 
 class NoteSerializer(serializers.ModelSerializer):
     content = serializers.CharField()
-    creator = UserSerializer()
+
+    def validate(self, attrs):
+        content = attrs.get('content')
+
+        # Validate the string length is no more than 30 chars
+        if len(content) > 30:
+            raise serializers.ValidationError("Having content greater than 30 chars is not allowed!")
+
+        pattern = r"[-!$%\^#&*()_+|~=`{}\[\]:\";'<>?,.\/]+"
+        is_not_valid = bool(re.findall(pattern, content))
+
+        if is_not_valid:
+            raise serializers.ValidationError("Non-supported chars found")
+
+        return attrs
 
     class Meta:
         model = Note
-        fields = ('content', 'creator')
+        fields = ('content', )
