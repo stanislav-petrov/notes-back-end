348386406272197ff6be718817758c669d98f9a6
stanislav-petrov <stanislavpetrov025@gmail.com>
2022-03-23 20:21:29 +0200

    Make note view accepts unauthorize requests

--- notes/views/note.py
+++ notes/views/note.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 
 from notes.models import Note
 from notes.serializers.note import NoteSerializer
@@ -7,3 +8,4 @@ from notes.serializers.note import NoteSerializer
 class NoteViewSet(viewsets.ModelViewSet):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
+    permission_classes = [AllowAny, ]
