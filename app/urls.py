5ce680071ce38c724de93ff9ed8a0d9370bbeb06
stanislav-petrov <stanislavpetrov025@gmail.com>
2022-03-23 19:49:48 +0200

    Rename main folder notes to apps

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,4 @@
-"""notes URL Configuration
+"""app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+
+from notes.views.note import NoteViewSet
+from notes.views.user import UserViewSet
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'notes', NoteViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
